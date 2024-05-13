9d60928f6ff418b086b94c27159fae46b11f4cb5
Harishsemwal <harishsemwal581@gmail.com>
2024-05-13 23:20:20 +0530

    javaScript
    javaScript

---
+++ 8_Arrays/index.js
@@ -0,0 +1,45 @@
+// const myArray = [0, 1, 2, 3, 4];
+// const myHero = ["Harish", "Prasad", "Semwal"];
+
+// const myArray = new Array(1, 2, 3);
+
+// +++++++++++ Methods ++++++++
+
+// myArray.push(3);
+// myHero.pop();
+
+// myArray.unshift(4); // add element in first
+// myArray.shift(); // remove element from first
+
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(9));
+
+// const newArray = myArray.join();
+// console.log(newArray);
+
+
+
+
+
+
+// // +++++++++++Map+++++++++++++
+
+
+// const myNumber = [1, 2, 3, 4, 5 ,6];
+
+// const newNum = myArray.map(() => {return num + 10})
+// console.log(newNUm);
+
+
+
+const myNum = [1, 2, 3];
+// const myTotal = myNum.reduce(function(acc, currVal){
+//     console.log(`The ${acc} ans currval ${currVal}`);
+//     return acc + currVal;
+// }, 0)
+
+
+const myTotal = myNum.reduce((acc, curval) => (acc + curval),0);
+
+console.log(myTotal);
