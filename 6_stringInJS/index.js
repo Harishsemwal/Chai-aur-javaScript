92df0e0665dcaa8b59a488455e0f32d6e1e1ccc6
Harishsemwal <harishsemwal581@gmail.com>
2024-05-13 22:16:13 +0530

    javaScript
    javaScript

---
+++ 6_stringInJS/index.js
@@ -0,0 +1,50 @@
+// var name = "Harish";
+
+// console.log(`my name is ${name} Semwal`);
+
+// console.log(name.toUpperCase());
+// console.log(name.charAt(2));
+
+
+// -----------------------------
+
+// console.log(name.indexof('i'));
+
+// const newString = name.substring(0, 4);
+// console.log(newString);
+
+// //reverse: also goes to negative
+// const anotherString = game.slice(-8, 4);
+// console.log(anotherString);
+
+
+
+
+
+// trim:
+
+// ----------------------------
+// 1-: trim start 
+// 2-: trim end 
+// const newStringOne = "   Harish   ";
+// console.log(newStringOne.trim());
+
+
+
+
+// replace:
+
+const url = "http:harishsemwal#gmail.com";
+
+console.log(url.replace('#', '.'));
+console.log(url.includes ('Harish'));  
+
+
+// split:
+
+const name = "har-ish";
+const value = name.split('-');
+console.log(value);
+
+const str = "harish";
+const reve = str.split.rev
---
+++ 7_Number_Math/index.js
@@ -0,0 +1,38 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+// const Numbers = 123.8964;
+// console.log(Number.toPrecision(4));
+
+// const hundred = 100000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// Max Value & Mini Value:
+
+// Number.MAX_VALUE;
+// Number.MIN_VALUE;
+
+// math Function:
+
+console.log(Math);
+console.log(Math.abs(-2));
+console.log(Math.round(5.64));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(5.4));
+
+console.log(Math.min(4, 6, 8));
+console.log(Math.max(2, 7, 9));
+
+
+console.log(Math.random());
+
+
