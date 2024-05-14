991c4aaec5e2dfee4e591335fece17f667fdf430
Harishsemwal <harishsemwal581@gmail.com>
2024-05-14 23:25:48 +0530

    JS
    JS

---
+++ 9_Classes_Objects/index.js
@@ -0,0 +1,10 @@
+const student = {
+    fullName : "Harish Semwal",
+    marks : 94.4,
+    printMarks : function(){
+        console.log("Marks: "+ this.marks);
+    },
+}
+
+// this: means here student.marks
+ 
